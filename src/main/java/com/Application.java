fa1b95058616944e88291060b7e79f4df65a3827
ShevchukI <ihShevchuk@gmail.com>
2019-07-06 15:18:45 +0300

    removed idea folder

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,9 +3,6 @@ package com;
 import com.controllers.windows.menu.WindowsController;
 import javafx.stage.Stage;
 
-/**
- * Created by User on 16.03.2019.
- */
 public class Application extends javafx.application.Application {
 
     public static void main(String[] args) throws Exception {
